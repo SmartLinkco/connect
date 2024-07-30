ff791713d9e5d077c511b6b8c84e8d823f67db55
Stephen Gyan Bimpong <91754696+SmartLinkco@users.noreply.github.com>
2024-07-30 17:05:58 +0000

    Add files via upload
    update gospel site

--- Gospel 1/script.js
+++ Gospel 1/script.js
@@ -1,24 +1,38 @@
-function appointment(){
-    window.location.href ='appointment.html';
-}
-
 function barbers(){
     window.location.href ='barbers.html';
+}
+
+function facebook(){
+    window.location.href ='https://www.facebook.com/share/QzcptwQENjskEg7K/?mibextid=LQQJ4d';
+}
+
+function instagram(){
+    window.location.href ='https://www.instagram.com/gospel1debarber?igsh=MXdtbGtodW00ZXVlZQ==';
+}
+
+function home(){
+    window.location.href ='gospel1barbershop.html';
+}
+
+function tiktok(){
+    window.location.href ='https://www.tiktok.com/@gospel1debarber?is_from_webapp=1&sender_device=pc';
+}
+
+function snapchat(){
+    window.location.href ='https://snapchat.com/t/UQx0OMIE';
+}
+
+function bookAppointment(){
+    window.location.href ='https://form.jotform.com/gospel1hairstudio/gosple-1-hair-studio';
 }
 
 function redirectToServices() {
     window.location.href = 'gospel1barbershop.html#servicess';
 }
 
-document.getElementById('bAppointment').addEventListener('click', function(event) {
-    document.getElementById('bookMain').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
-});
+document.getElementById('bAppointment').addEventListener('click', bookAppointment);
 
-document.getElementById('appointments').addEventListener('click', function() {
-    document.getElementById('bookMain').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
-});
+document.getElementById('appointments').addEventListener('click', bookAppointment);
 
 document.getElementById('closeBookMain').addEventListener('click', function() {
     document.getElementById('bookMain').style.display = 'none';
